50e9dc868821a5c215d1cae0bbca9157f9b31684
JeanBruneMello <jeanbrunemello@gmail.com>
2023-02-15 18:42:45 -0300

    backup do projeto app-galaxy

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ExoplanetsComponent } from './pages/exoplanets/exoplanets.component';
+import { HomeComponent } from './pages/home/home.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+
+const routes: Routes = [
+  {
+    path: "",
+    pathMatch: "full",
+    redirectTo: "home"
+  },
+  {
+    path: "home",
+    component: HomeComponent
+  },
+  {
+    path: "exoplanets",
+    component: ExoplanetsComponent
+  },
+  {
+    path: "exoplanets",
+    component: ExoplanetsComponent
+  },
+  {
+    path: "**",
+    component: NotFoundComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
